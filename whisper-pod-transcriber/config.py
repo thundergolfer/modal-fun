cbfd5163ab7a02057871447fa7b1fdb53373fe14
Jonathon Belotti <jonathon.i.belotti@gmail.com>
2022-09-30 16:18:04 -0400

    whisper-pod-transcribe: add commentary to config.py

--- whisper-pod-transcriber/config.py
+++ whisper-pod-transcriber/config.py
@@ -10,11 +10,20 @@ class ModelSpec:
 
 
 CACHE_DIR = "/cache"
+# Where downloaded podcasts are stored, by guid hash.
+# Mostly .mp3 files 50-100MiB.
 RAW_AUDIO_DIR = pathlib.Path(CACHE_DIR, "raw_audio")
+# Stores metadata of individual podcast episodes as JSON.
+# nb: Should probably be called EPISODES_METADATA_DIR.
 METADATA_DIR = pathlib.Path(CACHE_DIR, "metadata")
+# Crude boolean marker of a previously processed podcast.
 COMPLETED_DIR = pathlib.Path(CACHE_DIR, "completed")
+# Completed episode transcriptions. Stored as flat files with
+# files structured as '{guid_hash}-{model_slug}.json'.
 TRANSCRIPTIONS_DIR = pathlib.Path(CACHE_DIR, "transcriptions")
+# Searching indexing files, refreshed by scheduled functions.
 SEARCH_DIR = pathlib.Path(CACHE_DIR, "search")
+# Location of web frontend assets.
 ASSETS_PATH = pathlib.Path(__file__).parent / "web"
 # TODO: Remove
 podchaser_podcast_ids = {
