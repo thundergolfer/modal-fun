ca082fbc8e1ba679fbad395dc82c0c620f187371
Jonathon Belotti <jonathon.i.belotti@gmail.com>
2022-12-11 21:41:07 -0500

    fix: need email query param in confirm link

--- thundergolferdotcom-email-subs/email_subs/main.py
+++ thundergolferdotcom-email-subs/email_subs/main.py
@@ -190,7 +190,7 @@ def send_confirmation_email(email: str):
 
     subscriber = store.create_sub(email=email)
     code = subscriber.confirm_code
-    confirm_link = f"https://thundergolfer--{app_name}-web.modal.run/confirm?code={code}"
+    confirm_link = f"https://thundergolfer--{app_name}-web.modal.run/confirm?email={subscriber.email}code={code}"
     sender = emailer.GmailSender(creds)
     copy = email_copy.confirm_subscription_email(confirm_link)
     emailer.send(
