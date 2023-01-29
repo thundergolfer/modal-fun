ac6452d5fce4fbd22df840ca644cd7902d2075df
Jonathon Belotti <jonathon.i.belotti@gmail.com>
2023-01-28 22:12:35 -0500

    wip: can now send confirm email in test and see it in fetch results!

--- thundergolferdotcom-email-subs/tests/integration/end_to_end.py
+++ thundergolferdotcom-email-subs/tests/integration/end_to_end.py
@@ -1,7 +1,9 @@
 import base64
+import time
 import uuid
 import pathlib
 from datetime import datetime, timezone
+from typing import NamedTuple
 
 import modal
 
@@ -13,15 +15,26 @@ from email_subs.main import (
     DB_PATH,
     image,
     fetch_fresh_gmail_creds_from_env,
+    send_confirmation_email,
     web,
 )
 
 test_app_name = app_name + "-test"
+test_image = image.pip_install("httpx")
 volume = modal.SharedVolume()  # Doesn't need to be persisted.
-stub = modal.Stub(name=test_app_name, image=image)
+stub = modal.Stub(name=test_app_name, image=test_image)
 
 
 web_app_handle = stub.asgi(shared_volumes={CACHE_DIR: volume})(web.get_raw_f())
+stub.function(
+    secret=modal.Secret.from_name("gmail"),
+    shared_volumes={CACHE_DIR: volume},
+)(send_confirmation_email.get_raw_f())
+
+
+class Message(NamedTuple):
+    snippet: str
+    body: str
 
 
 @stub.function(shared_volumes={CACHE_DIR: volume})
@@ -53,14 +66,25 @@ def fetch_recent_emails(n: int = 3) -> list[str]:
             .get(userId="me", id=m["id"], format="full")
             .execute()
         )
-        body_text = base64.b64decode((r["payload"]["body"]["data"]))
-        message_bodies.append(body_text)
+        if "data" not in r["payload"]["body"]:
+            print("Email had no body data.")
+            body_text = ""
+        else:
+            body_text = base64.b64decode((r["payload"]["body"]["data"]))
+        message_bodies.append(Message(body=body_text, snippet=r["snippet"]))
     return message_bodies
 
 
 # TODO: Use @pytest.mark.modal when that's supported.
 def test_end_to_end():
+    import httpx
+
+    # The personal email associated with my Gmail auth.
+    test_email_addr = "jonathon.i.belotti@gmail.com"
+
     with stub.run():
+        print(stub.registered_functions)
+
         test_web_url = web_app_handle.web_url
         print(test_web_url)
 
@@ -69,11 +93,18 @@ def test_end_to_end():
         create_test_db.call()
 
         # 2. Call /subscribe with email
+        print("Hitting /subscribe endpoint.")
+        httpx.get(f"{test_web_url}/subscribe?email={test_email_addr}")
+
+        time.sleep(10)  # wait for email to be sent.
 
         # 3. Read email from my Gmail and get confirmation link
         messages = fetch_recent_emails.call()
 
         assert len(messages) > 0
+
+        for msg in messages:
+            print(msg)
 
         # 4. GET the confirmation link to confirm subscription
 
