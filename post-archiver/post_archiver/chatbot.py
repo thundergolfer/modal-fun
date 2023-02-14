4e5a38a9359b42bfde135c60e01d1fa325fc3b62
Jonathon Belotti <jonathon.i.belotti@gmail.com>
2023-02-13 22:28:59 -0500

    Edit the GPT prompt

--- post-archiver/post_archiver/chatbot.py
+++ post-archiver/post_archiver/chatbot.py
@@ -99,12 +99,12 @@ def get_new_chain1(vectorstore):
         template=">Example:\nContent:\n---------\n{page_content}\n----------\nSource: {source}",
         input_variables=["page_content", "source"],
     )
-    template = """You are an AI assistant for the open source library LangChain. The documentation is located at https://langchain.readthedocs.io.
-You are given the following extracted parts of a long document and a question. Provide a conversational answer with a hyperlink to the documentation.
+    template = """You are an AI assistant for a software engineer named Jonathon. His blog is https://thundergolfer.com.
+You are given the following extracted parts of a long document and a question. Provide a conversational answer to the question.
 You should only use hyperlinks that are explicitly listed as a source in the context. Do NOT make up a hyperlink that is not listed.
 If the question includes a request for code, provide a code block directly from the documentation.
 If you don't know the answer, just say "Hmm, I'm not sure." Don't try to make up an answer.
-If the question is not about LangChain, politely inform them that you are tuned to only answer questions about LangChain.
+If the question is not about Jonathon, politely inform them that you are tuned to only answer questions about Jonathon.
 Question: {question}
 =========
 {context}
